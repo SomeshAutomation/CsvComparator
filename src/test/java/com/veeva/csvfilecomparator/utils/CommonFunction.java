a56e693c28eff3b2544637dafb4b433812c41c93
Somesh <diwanjisomeshtp@gmail.com>
2025-05-29 23:25:22 -0400

    Update - Common Funciton Class added - Updated test data files and config properties details - Added soft assertiong for the test case #1234

---
+++ src/test/java/com/veeva/csvfilecomparator/utils/CommonFunction.java
@@ -0,0 +1,43 @@
+package com.veeva.csvfilecomparator.utils;
+
+import com.veeva.csvfilecomparator.runner.Hooks;
+import io.qameta.allure.Allure;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.testng.asserts.SoftAssert;
+
+import java.io.*;
+import java.util.List;
+
+
+public class CommonFunction {
+
+    public List<CSVRecord> getCSVRecordList(String csvFileName) {
+        Reader filereader = null;
+        try {
+            filereader = new FileReader(new File(Hooks.PROPERTIES.getProperty("testDataPath") + csvFileName));
+            return CSVFormat.DEFAULT.parse(filereader).getRecords();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public SoftAssert compareCSVRecord(CSVRecord expectedRecord, CSVRecord actualRecord, SoftAssert softAssert) {
+        String[] expectedValues = expectedRecord.values();
+        String[] actualValues = actualRecord.values();
+        int minLength = Math.min(expectedValues.length, actualValues.length);
+        softAssert.assertEquals(actualRecord.getComment(), expectedRecord.getComment(), "Comments Mismatch for record number " + expectedRecord.getRecordNumber() + " : ");
+        softAssert.assertEquals(actualValues.length, expectedValues.length, "Expected number of values in the record number " + expectedRecord.getRecordNumber() + " mismatch");
+        for (int i = 0; i < minLength; i++) {
+            softAssert.assertEquals(actualValues[i], expectedValues[i], "Mismatch for record number " + expectedRecord.getRecordNumber() + " at column " + (i + 1));
+        }
+        return softAssert;
+    }
+
+    public void attachText(String name, String content) {
+        Allure.addAttachment(name, "text/plain", content);
+    }
+}
--- src/test/resources/features/CsvComparatorTest.feature
+++ src/test/resources/features/CsvComparatorTest.feature
@@ -1,13 +1,12 @@
-@CsvComparision
+@CsvComparison
 Feature: Compare two csv files and identify differences
 
-  @TestID-1234 @FeatureName
+  @TestID-1234
   Scenario Outline: Identify the differences between two CSV files - with column-to-column approach
     Given user has csv file named "<filename1>"
     And user has another csv file named "<filename2>"
     When user compares two csv files
     Then the system should identify differences
     Examples:
-      | entity | filename1  | filename2           |
-      | TOR    | TEAM   | Toronto Raptors |
-      | NHL    | LEAGUE | NHL             |
+      | filename1 | filename2 |
+      | file1.csv | file2.csv |
